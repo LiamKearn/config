f1df27453d4f0a01fa7890ee14169c2626f4b819
LiamKearn <76269376+LiamKearn@users.noreply.github.com>
2022-07-16 20:49:55 +1000

    Init: Start tracking dotfiles again

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+set -Ux EDITOR nvim
+set -Ux BROWSER chrome
+set -Ux NAME "Liam Kearney"
+set -Ux EMAIL liam@sproutlabs.com.au
+set -Ux TZ Australia/Hobart
+# I don't use -F because if I want cat I'll use cat. :-)
+# I don't use -X because if I want cat I'll use cat. :-)
+set -Ux LESS "-R"
+
+fish_add_path /usr/local/bin/nvim-osx64/bin
+
+abbr -a l 'ls -1sha'
+abbr -a ll 'ls -lsha'
+abbr -a shi 'ssh imprint'
+abbr -a shit 'ssh imprint -t "tmux a; bash -l"'
+abbr -a lzg 'lazygit'
+abbr -a lzgc 'lazygit ~/.cfg'
+abbr -a lzd 'lazydocker'
+abbr -a tunconnect 'osascript ~/.tunnelblick/tunnelconnect.scpt'
+abbr -a tunstop 'osascript ~/.tunnelblick/tunnelstop.scpt'
+
+alias vi=nvim
+alias vic='nvim $HOME/.config/fish/config.fish'
+alias vim=nvim
+alias findinpath="echo $PATH | sed 's/:/\n/g' | xargs -I % sh -c 'ls -ldhA —color %/* 2>/dev/null;' | fzf"
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
---
+++ README.md
@@ -0,0 +1,36 @@
+# Configs
+> It's dangerous to type alone take these..
+
+# Env
+This is the OSX branch of my configs.
+I use a mix of a Macbook and a Ubuntu (pop-os) machine for development.
+
+# Primary software
+ - Code (Would use VIM or jIDEs but code's remote support is the only viable option really)
+ - Chrome
+  - Vimium plugin
+  - Refined GH
+  - Select2 Anywhere
+  - Tampermonkey
+  - UBlock Origin
+ - Rectangle
+ - Easy Move+Resize
+ - Alt-tab
+ - Alacritty
+  - TMUX
+  - NVIM
+  - Lazygit
+  - Lazydocker
+  - Bat
+  - RipGrep
+ - Tunnelblick VPN client
+ - Alfred
+ - Limelight active window border
+  - Dock disabled
+  - Menubar on autohide with KB shortcut
+
+# Install
+```fish
+git clone --separate-git-dir=$HOME/.cfg git@github.com:LiamKearn/config.git ~
+```
+
