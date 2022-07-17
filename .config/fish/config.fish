8af9ba54277edc367724b52cf0f547c7478bcab0
LiamKearn <76269376+LiamKearn@users.noreply.github.com>
2022-07-17 17:56:11 +1000

    Feat: Config, add missing aliases.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,14 +14,18 @@ abbr -a lzd 'lazydocker'
 abbr -a tunconnect 'osascript $HOME/.config/tunnelblick/tunnelconnect.scpt'
 abbr -a tunstop 'osascript $HOME/.config/tunnelblick/tunnelstop.scpt'
 abbr -a src 'source $HOME/.config/fish/config.fish'
+abbr -a tma 'tmux a'
 
 alias readlink='/usr/local/bin/greadlink'
+alias cat='bat'
 alias vi='nvim'
 alias vim='nvim'
 alias vic='nvim $HOME/.config/fish/config.fish'
 alias vie='nvim $HOME/.config/fish/public_env.fish'
 alias viep='nvim $HOME/.config/fish/private_env.fish'
 alias findinpath="echo $PATH | sed 's/:/\n/g' | xargs -I % sh -c 'ls -ldhA —color %/* 2>/dev/null;' | fzf"
+alias findinstash="git stash list | awk '{print \$1}' | sed 's/stash@{//g' | sed 's/}://g' | xargs -t -I '{}' git stash show {} | rg"
+alias findinhistory="history | fzf | awk '{\$1=\"\"; print \$0}' | xargs -i{} -p sh -c {}"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias rg='rg --no-messages'
 
