13cea57b9a661c9fc4ec378e6f965b77447c213d
lkearn <liam@sproutlabs.com.au>
2022-08-04 12:32:22 +1000

    Fix: Add the platforms tools back as well.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ fish_add_path /usr/local/bin/nvim-osx64/bin
 fish_add_path $HOME/.bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $ANDROID_SDK_ROOT/tools/bin
+fish_add_path $ANDROID_SDK_ROOT/platform-tools
 
 abbr -a l 'ls -1'
 abbr -a ll 'ls -la'
