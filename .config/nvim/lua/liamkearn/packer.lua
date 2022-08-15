de396f757fbca31486ad86af2234bc0494de8027
lkearn <liam@sproutlabs.com.au>
2022-08-15 23:13:32 +1000

    Chore: Cleanup deps.

--- .config/nvim/lua/liamkearn/packer.lua
+++ .config/nvim/lua/liamkearn/packer.lua
@@ -1,13 +1,21 @@
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use 'sainnhe/gruvbox-material'
-  use 'nvim-telescope/telescope.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'dag/vim-fish'
-  use 'nvim-lua/plenary.nvim'
+    use 'sainnhe/gruvbox-material'
+    use 'nvim-telescope/telescope.nvim'
+    use 'dag/vim-fish'
+    use 'nvim-lua/plenary.nvim'
+
+    -- Language support stuff.
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/nvim-cmp'
+
+    use("nvim-treesitter/nvim-treesitter", {
+        run = ":TSUpdate"
+    })
+    use ("nvim-treesitter/playground", {
+        run = ":TSInstall query"
+    })
 end)
