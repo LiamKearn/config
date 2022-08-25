147b1d763f60124654c8b4599a454ab111339fde
lkearn <liam@sproutlabs.com.au>
2022-08-25 17:16:58 +1000

    Feat: Improve ergonomics of docker utilities by failing fast instead of writing half-finished commands to prompt.

--- .config/fish/functions/doexec.fish
+++ .config/fish/functions/doexec.fish
@@ -1,7 +1,13 @@
 function doexec
-    set id (docker ps | sed '1d' | awk '{print $1, $2}' | fzf | awk '{print $1}')
+    set id (docker ps --format '{{.Label "com.docker.compose.project"}}\t{{ .ID }}\t{{ .Image }}' | fzf | awk '{print $2}')
+    if not test -n "$id"
+        return
+    end
     set shell_list "/bin/bash" "/bin/zsh" "redis-cli"
     set chosen_shell (printf '%s\n' $shell_list | fzf)
+    if not test -n "$chosen_shell"
+        return
+    end
     commandline -r -- "docker exec -it $id $chosen_shell"
 end
 
--- .config/fish/functions/dolog.fish
+++ .config/fish/functions/dolog.fish
@@ -1,5 +1,8 @@
 function dolog
-    set id (docker ps | sed '1d' | awk '{print $1, $2}' | fzf | awk '{print $1}')
+    set id (docker ps --format '{{.Label "com.docker.compose.project"}}\t{{ .ID }}\t{{ .Image }}' | fzf | awk '{print $2}')
+    if not test -n "$id"
+        return
+    end
     commandline -r -- "docker logs -f --since 0m $id"
 end
 
