73609dac1317287f2626c66cb1a57fcc007f6e2a
lkearn <liam@sproutlabs.com.au>
2022-12-08 18:55:03 +1100

    Feat: phpoptdiff, Add a header to describe opt levels and reference for codes.

--- .config/fish/functions/phpoptdiff.fish
+++ .config/fish/functions/phpoptdiff.fish
@@ -2,12 +2,22 @@ function phpoptdiff -d "Displays opcache diffs"
     if not test -n "$argv[1]"
         return;
     end
+    set header 'https://www.npopov.com/2022/05/22/The-opcache-optimizer.html
+---
+0x10000: Dump before optimizer.
+0x20000: Dump after optimizer.
+0x40000: Dump before CFG optimizations.
+0x80000: Dump after CFG optimizations.
+0x200000: Dump before SSA optimizations.
+0x400000: Dump after SSA optimizations.
+---
+'
     set optlevels 0x10000 0x20000 0x40000 0x80000 0x200000 0x400000
-    set startlevel (printf '%s\n' $optlevels | fzf --prompt 'Choose a starting optimisation level: ' --layout=reverse)
+    set startlevel (printf '%s\n' $optlevels | fzf --prompt 'Choose a starting optimisation level: ' --layout=reverse --header $header --header-first)
     if not test -n "$startlevel"
         return;
     end
-    set endlevel (printf '%s\n' $optlevels | fzf --prompt 'Choose a optimised level: ' --layout=reverse --tac)
+    set endlevel (printf '%s\n' $optlevels | fzf --prompt 'Choose a optimised level: ' --layout=reverse --tac --header $header --header-first)
     if not test -n "$endlevel"
         return;
     end
