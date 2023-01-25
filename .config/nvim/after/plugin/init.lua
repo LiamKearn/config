1f76b5efd6ea246f00b1ef010cb8f9142438b888
lkearn <liam@sproutlabs.com.au>
2023-01-25 14:03:31 +1100

    Fix: Show hidden files.

--- .config/nvim/after/plugin/init.lua
+++ .config/nvim/after/plugin/init.lua
@@ -1,8 +1,17 @@
-require('oil').setup()
+require('oil').setup({
+    view_options = {
+        show_hidden = true,
+    }
+})
 
 require('telescope').setup{
     defaults = {
         file_ignore_patterns = {},
+    },
+    pickers = {
+        find_files = {
+            hidden = true
+        }
     }
 }
 
