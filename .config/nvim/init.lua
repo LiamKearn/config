d65b52c9cbbf9030b824281c097896217437fd67
lkearn <liam@sproutlabs.com.au>
2023-02-23 17:31:00 +1100

    Feat: NVIM, Hide lazy reload messages.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -72,7 +72,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup('plugins', {
+    change_detection = {
+        enabled = true,
+        notify = false,
+    },
+})
 
 local autocmd = vim.api.nvim_create_autocmd
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
