48de28583159929f701b3540835028583aea7d24
lkearn <liam@sproutlabs.com.au>
2023-02-23 20:55:04 +1100

    Refactor: NVIM, Clean up some of the color code, still slow and stupid but better.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,11 +40,11 @@ vim.opt.smartindent = true
 -- From Greg Hurrell's config.
 vim.opt.list = true
 vim.opt.listchars = {
-  nbsp = '⦸', -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
-  extends = '»', -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
-  precedes = '«', -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
-  tab = '▷⋯', -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
-  trail = '•', -- BULLET (U+2022, UTF-8: E2 80 A2)
+  nbsp = '⦸',
+  extends = '»',
+  precedes = '«',
+  tab = '▷⋯',
+  trail = '•',
 }
 
 -- GUI
@@ -57,6 +57,9 @@ vim.opt.splitright = true
 
 -- Filetype mappings.
 vim.g.filetype_ss = 'html'
+
+vim.cmd('colorscheme slate')
+vim.api.nvim_set_hl(0, 'ColorColumn', { bg = "#333333", fg = "none" })
 
 -- Install lazy.
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
@@ -95,45 +98,33 @@ end
 local autocmd = vim.api.nvim_create_autocmd
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 
+-- TODO: All the reloading here is evil.
+function toggleScheme()
+    if (vim.g.colors_name == 'slate') then
+        vim.cmd('colorscheme habamax')
+    else
+        vim.cmd('colorscheme slate')
+    end
+
+    -- Any hightlight changes need to be reloaded.
+    vim.api.nvim_set_hl(0, 'ColorColumn', { bg = "#333333", fg = "none" })
+    require('lualine').setup({
+        options = {
+            icons_enabled = false,
+            theme = 'gruvbox-material',
+            component_separators = '|',
+            section_separators = '',
+        }
+    })
+end
+autocmd('InsertEnter', { callback = toggleScheme })
+autocmd('InsertLeave', { callback = toggleScheme })
+
 autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
   group = highlight_group,
-  pattern = '*',
-})
-
-vim.cmd('colorscheme ' .. 'slate')
-
-autocmd('InsertEnter', {
-    pattern = '*',
-    callback = function()
-        -- TODO: All the reloading here is evil.
-        vim.cmd('colorscheme habamax')
-        require('lualine').setup({
-            options = {
-                icons_enabled = false,
-                theme = 'gruvbox-material',
-                component_separators = '|',
-                section_separators = '',
-            }
-        })
-    end
-})
-
-autocmd('InsertLeave', {
-    pattern = '*',
-    callback = function()
-        vim.cmd('colorscheme slate')
-        require('lualine').setup({
-            options = {
-                icons_enabled = false,
-                theme = 'gruvbox-material',
-                component_separators = '|',
-                section_separators = '',
-            }
-        })
-    end
 })
 
 -- Trash all the buffers!
