c48de1818c2d422ecc6cea0855b992dad2755ced
lkearn <liam@sproutlabs.com.au>
2023-04-25 20:12:54 +1000

    Refactor: Adapt explain script to dynamically operate on strings or space interpolated arguments.

--- .config/fish/functions/explain.fish
+++ .config/fish/functions/explain.fish
@@ -1,8 +1,9 @@
 function explain -d "Open a browser with the explainshell.com page for the given command"
     if not test -n "$argv[1]"
+        echo "Usage: explain <command>"
         return;
     end
     set base_url 'https://explainshell.com/explain?cmd='
-    set cmd (echo $argv[1] | urlencode)
+    set cmd (echo $argv[1..-1] | urlencode)
     open "$base_url$cmd"
 end
