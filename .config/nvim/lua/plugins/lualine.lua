643a1262542db8498ac7481ba400715a2c0f84b0
lkearn <liam@sproutlabs.com.au>
2023-05-04 21:24:30 +1000

    Feat: NVIM, Switch to catppuccin.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     opts = {
         options = {
             icons_enabled = false,
-            theme = 'gruvbox-material',
+            theme = 'catppuccin',
             component_separators = '|',
             section_separators = '',
         },
