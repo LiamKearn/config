f96db9949a117f245cc70e373a7c8a6962f05216
lkearn <liam@sproutlabs.com.au>
2023-05-09 21:15:42 +1000

    Chore: Brew, Move brewfile.

--- .config/fish/functions/brew.fish
+++ .config/fish/functions/brew.fish
@@ -1,5 +1,5 @@
 function brew
-    set brewfile_location $HOME/Brewfile
+    set brewfile_location $XDG_CONFIG_HOME/brew/Brewfile
     if command brew $argv
         command brew bundle dump --file=$brewfile_location --force
         printf "Dumped brewfile to %s\n" $brewfile_location
