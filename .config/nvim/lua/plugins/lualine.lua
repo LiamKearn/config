676572b406ceda1011dbdc3c49fec0919a41bc1a
lkearn <liam@sproutlabs.com.au>
2023-06-17 22:45:10 +1000

    Feat: NVIM, Lualine, Display selection counts now that I'm using cmdheight 0.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,12 +8,26 @@ return {
             section_separators = '',
         },
         sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'diagnostics'},
-            lualine_c = {'filename'},
+            lualine_a = { 'mode' },
+            lualine_b = { 'diagnostics' },
+            lualine_c = { 'filename' },
             lualine_y = {},
-            lualine_x = {'encoding', 'filetype'},
-            lualine_z = {'location'}
+            lualine_x = { 'encoding', 'filetype' },
+            lualine_z = {
+                {
+                    function()
+                        local starts = vim.fn.line("v")
+                        local ends = vim.fn.line(".")
+                        local count = starts <= ends and ends - starts + 1 or starts - ends + 1
+                        local wc = vim.fn.wordcount()
+                        return count .. ":" .. wc["visual_chars"]
+                    end,
+                    cond = function()
+                        return vim.fn.mode():find("[Vv]") ~= nil
+                    end,
+                },
+                'location'
+            }
         }
     }
 }
