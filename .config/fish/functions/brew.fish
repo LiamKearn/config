a14757d128bce20033bbc6c22177a4aec310be0b
LiamKearn <contact@liamkearn.me>
2023-11-21 21:28:19 +1100

    Feat: Brew, Don't dump brewfile on immutable commands

--- .config/fish/functions/brew.fish
+++ .config/fish/functions/brew.fish
@@ -1,4 +1,10 @@
 function brew
+    set -l disabled_commands "search" "bundle" "bundle dump" "info"
+    if contains $argv[1] $disabled_commands
+        command brew $argv
+        return
+    end
+
     set brewfile_location $XDG_CONFIG_HOME/brew/Brewfile
     if command brew $argv
         command brew bundle dump --file=$brewfile_location --force
