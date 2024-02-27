9c662e5f7e19713e8ea465b166198fabde215946
LiamKearn <contact@liamkearn.me>
2024-02-27 23:23:57 +1100

    Move WGET history

--- .config/fish/public_env.fish
+++ .config/fish/public_env.fish
@@ -41,6 +41,7 @@ set -Ux MAIL $HOME/mbox
 set -Ux DOCKER_BUILDKIT 1
 set -Ux HOMEBREW_NO_AUTO_UPDATE 1
 set -Ux PYTHONSTARTUP "$XDG_CONFIG_HOME/python/init.py"
+set -Ux WGETRC "$XDG_CONFIG_HOME/wget/wgetrc"
 
 switch (uname)
     case Darwin
