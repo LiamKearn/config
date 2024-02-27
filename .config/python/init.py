78f807e63034024d962a417f13c066a0bdcb09fc
LiamKearn <contact@liamkearn.me>
2024-02-27 23:52:37 +1100

    Add truthy filter to python

--- .config/python/init.py
+++ .config/python/init.py
@@ -2,6 +2,7 @@ import atexit
 import os
 import readline
 from pathlib import Path
+from functools import partial
 
 MAX_HIST_LEN = 10000
 
@@ -16,5 +17,7 @@ if not os.path.exists(histfile):
 readline.read_history_file(histfile)
 readline.set_history_length(MAX_HIST_LEN)
 atexit.register(readline.write_history_file, histfile)
+
+truthy = partial(filter, None)
 
 print(f'[init.py] Utilising {histfile} for history.')
