c7e9848cd4f78d4255b4963069925d31e4285be8
LiamKearn <contact@liamkearn.me>
2024-09-26 00:04:49 +1000

    Hide new tab button in firefox

--- Library/Application Support/Firefox/profile/user.js
+++ Library/Application Support/Firefox/profile/user.js
@@ -1,3 +1,5 @@
+user_pref('legacyUserProfileCustomizations.stylesheets', true);
+user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
 user_pref('sidebar.revamp', true);
 user_pref('sidebar.verticalTabs', true);
 user_pref('browser.fullscreen.autohide', true);
