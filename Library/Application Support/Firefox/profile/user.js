5891b237a4f9777dc030a5ff7f8cf213d53ef968
LiamKearn <contact@liamkearn.me>
2024-09-25 23:27:56 +1000

    Firefox config changes

--- Library/Application Support/Firefox/profile/user.js
+++ Library/Application Support/Firefox/profile/user.js
@@ -4,3 +4,13 @@ user_pref('browser.fullscreen.autohide', true);
 user_pref('full-screen-api.ignore-widgets', true);
 user_pref('browser.warnOnQuitShortcut', false);
 user_pref('browser.tabs.hoverPreview.enabled', false);
+user_pref('browser.tabs.insertAfterCurrent', false);
+user_pref('browser.tabs.insertRelatedAfterCurrent', false);
+user_pref('sidebar.main.tools', 'aichat');
+user_pref('browser.ml.chat.enabled', true);
+user_pref('browser.ml.chat.hideLocalhost', true);
+user_pref('browser.ml.chat.provider', 'https://chatgpt.com');
+user_pref('browser.ml.chat.shortcuts', false);
+user_pref('browser.toolbars.bookmarks.visibility', 'never');
+user_pref('browser.aboutConfig.showWarning', false);
+
