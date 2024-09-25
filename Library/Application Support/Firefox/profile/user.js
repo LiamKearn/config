f3e247c39cacc65aa8d51a047ebca679794bba99
LiamKearn <contact@liamkearn.me>
2024-09-25 21:57:24 +1000

    Switch to Firefox after Arc CVE

---
+++ Library/Application Support/Firefox/profile/user.js
@@ -0,0 +1,6 @@
+user_pref('sidebar.revamp', true);
+user_pref('sidebar.verticalTabs', true);
+user_pref('browser.fullscreen.autohide', true);
+user_pref('full-screen-api.ignore-widgets', true);
+user_pref('browser.warnOnQuitShortcut', false);
+user_pref('browser.tabs.hoverPreview.enabled', false);
