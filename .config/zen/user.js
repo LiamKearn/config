1ec634104ab2964ef1a4fc6076276b34888524a7
LiamKearn <contact@liamkearn.me>
2025-05-11 17:02:45 +1000

    Configure zen browser

---
+++ .config/zen/user.js
@@ -0,0 +1,14 @@
+user_pref('zen.tabs.show-newtab-vertical', false);
+user_pref('zen.view.compact.hide-toolbar', true);
+user_pref('zen.view.compact.hide-tabbar', false);
+user_pref('zen.view.compact.toolbar-flash-popup', true);
+user_pref('zen.view.compact.toolbar-flash-popup.duration', '2000')
+user_pref('zen.glance.enabled', false);
+user_pref('zen.theme.accent-color', '#dec663');
+
+
+user_pref('browser.fullscreen.autohide', true);
+user_pref('full-screen-api.ignore-widgets', true);
+user_pref('browser.warnOnQuitShortcut', false);
+user_pref('browser.aboutConfig.showWarning', false);
+
