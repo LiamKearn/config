f4ae68f1aa1cca6fa50c9a9a25358f4c2ad1cf54
LiamKearn <contact@liamkearn.me>
2025-05-15 19:10:42 +1000

    Make sure zen doesn't update automatically

--- .config/zen/user.js
+++ .config/zen/user.js
@@ -6,7 +6,7 @@ user_pref('zen.view.compact.toolbar-flash-popup.duration', '2000')
 user_pref('zen.glance.enabled', false);
 user_pref('zen.theme.accent-color', '#dec663');
 
-
+user_pref('app.update.auto', false);
 user_pref('browser.fullscreen.autohide', true);
 user_pref('full-screen-api.ignore-widgets', true);
 user_pref('browser.warnOnQuitShortcut', false);
