3b3e52d2d769369c19999e9f4d3ca3acbe61877f
LiamKearn <contact@liamkearn.me>
2025-06-14 09:22:12 +1000

    Treesitter disable playground and textobject stuff

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,13 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    event = { "BufReadPost", "BufNewFile" },
+    lazy = false,
     keys = {
         { "<c-space>", desc = "Increment selection" },
         { "<bs>",      desc = "Schrink selection",  mode = "x" },
     },
     opts = {
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true },
         indent = { enable = true },
-        playground = {
-            enable = false,
-            disable = {},
-            updatetime = 25
-        },
         ensure_installed = {
             'bibtex',
             'cmake',
@@ -36,50 +28,16 @@ return {
             'markdown',
             'json',
             'json5',
-            'vue'
+            'vue',
         },
         incremental_selection = {
             enable = true,
             keymaps = {
+                scope_incremental = false,
                 init_selection = "<C-space>",
                 node_incremental = "<C-space>",
-                scope_incremental = "<nop>",
                 node_decremental = "<bs>",
-            },
-        },
-        textobjects = {
-            move = {
-                enable = true,
-                set_jumps = true,
-                goto_next_start = {
-                    ["]p"] = "@parameter.inner",
-                    ["]m"] = "@function.outer",
-                    ["]]"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["]M"] = "@function.outer",
-                    ["]["] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[p"] = "@parameter.inner",
-                    ["[m"] = "@function.outer",
-                    ["[["] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[M"] = "@function.outer",
-                    ["[]"] = "@class.outer",
-                },
-            },
-        }
-    },
-    init = function()
-        vim.filetype.add({
-            extension = {
-                ss = 'html'
             }
-        })
-    end,
-    config = function(_, opts)
-        require('nvim-treesitter.configs').setup(opts)
-    end
+        }
+    }
 }
