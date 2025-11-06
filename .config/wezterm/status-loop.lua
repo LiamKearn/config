4ade99fe4bcfb63c8f4c12b5bc2f4cb123f39505
LiamKearn <contact@liamkearn.me>
2025-11-06 14:03:37 +1100

    Delete wezterm configuration

--- .config/wezterm/status-loop.lua
+++
@@ -1,5 +0,0 @@
-local wezterm = require 'wezterm';
-
-wezterm.on('update-status', function(window, _)
-    require('status-hooks/last-tab')(window)
-end);
--- .config/wezterm/toasty.lua
+++
@@ -1,6 +0,0 @@
-local wezterm = require 'wezterm'
-
-wezterm.on('scollback-cleared', function(window, _)
-  window:toast_notification('wezterm', 'scrollback buffer cleared', nil, 200)
-end)
-
